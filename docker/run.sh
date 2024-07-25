3c96068065f09c38c094190c98242d33aa84f458
prodasl <prodasl@hotmail.com>
2024-07-25 00:03:06 -0500

    Changes to dockerize the app

---
+++ docker/run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#YOU NEED TO EXPORT THE WALLET_ADDRESS AND WALLET_PK FIRST
+#E.g.
+#export WALLET_ADDRESS=your_wallet_address
+#export WALLET_PK=your_wallet_pk
+
+docker run -e REACT_APP_WALLET_ADDRESS=$WALLET_ADDRESS -e REACT_APP_WALLET_PK=$WALLET_PK -dp 3000:3000 usdt-batch-transfer:latest
