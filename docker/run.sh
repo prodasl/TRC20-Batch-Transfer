236eedd2ad994eb3dbbbb7067139a839d8a40a2e
prodasl <prodasl@hotmail.com>
2024-07-28 18:11:03 -0500

    Changes to accept the environment variable of prod or test to configure the environment automatically

--- docker/run.sh
+++ docker/run.sh
@@ -5,4 +5,4 @@
 #export WALLET_ADDRESS=your_wallet_address
 #export WALLET_PK=your_wallet_pk
 
-docker run -e REACT_APP_WALLET_ADDRESS=$WALLET_ADDRESS -e REACT_APP_WALLET_PK=$WALLET_PK -dp 3000:3000 paulrodas/usdt-batch-transfer:latest
+docker run -e REACT_APP_WALLET_ADDRESS=$WALLET_ADDRESS -e REACT_APP_WALLET_PK=$WALLET_PK REACT_APP_ENVIRONMENT='prod' -dp 3000:3000 paulrodas/usdt-batch-transfer:latest
